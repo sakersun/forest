2fe5ee196d5fce08149c1f69d72fe320a0b9cab0
sakersun <sakersun@163.com>
2022-06-30 21:54:26 +0800

    dragon integrate with log4j2 and slf4j

--- dragon/src/test/java/com/sakersun/forest/AppTest.java
+++ dragon/src/test/java/com/sakersun/forest/AppTest.java
@@ -1,18 +1,18 @@
 package com.sakersun.forest;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest {
+class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue() {
+    void shouldAnswerWithTrue() {
         assertTrue(true);
     }
 }
