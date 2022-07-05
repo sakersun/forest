e66ca576ed95892305ccf8809e16f67587402cc9
sakersun <sakersun@163.com>
2022-07-05 16:55:58 +0800

    create flowable project basic structure

---
+++ flowable/src/test/java/com/sakersun/forest/flowable/FlowableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakersun.forest.flowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
