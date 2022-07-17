28134caad63051d674fb86ff8a3bf9cce420e7e7
sakersun <sakersun@163.com>
2022-07-17 23:07:27 +0800

    path change

--- phoenix/__init__.py
+++ phoenix/__init__.py
@@ -1,6 +1,2 @@
-from importlib.resources import path
-import os
 import sys
-module_path = os.path.abspath('.')
-if module_path not in sys.path:
-    sys.path.append(module_path)
+sys.path.append('.')
